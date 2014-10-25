88613a08c2a42f77eb16e908d90646889a1a5456
Alexander <alexandersvae@gmail.com>
2014-10-24 23:58:51 -0700

    [#81247222] Added descroption to the challenge model

--- Compete/db/schema.rb
+++ Compete/db/schema.rb
@@ -11,12 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141024221235) do
+ActiveRecord::Schema.define(version: 20141025065428) do
 
   create_table "challenges", force: true do |t|
     t.string   "name"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.text     "description"
   end
 
 end
