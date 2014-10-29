8d1372a02137a13c45c535157584689b78d308fd
Eirik Fosser <eirikfosser@gmail.com>
2014-10-28 20:46:20 -0700

    Added layout to comments and New comment button on challenges

--- app/models/challenge.rb
+++ app/models/challenge.rb
@@ -1,6 +1,13 @@
 class Challenge < ActiveRecord::Base
+  has_many :comments
   validates :name, presence: true, format: { with: /\A[a-zA-Z]+\z/, message: "only allows letters"}
   validates_uniqueness_of :name
   validates_length_of :name,
                       :minimum => 4
+
+  def root_comments
+  	comments
+  end
+
+  
 end
