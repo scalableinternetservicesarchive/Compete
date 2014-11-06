ec7ca41ef64dcc45a3eef941c10963900c4252bf
Jonas Toreskaas <jtoreskaas@gmail.com>
2014-11-05 18:34:08 -0800

    [Fix #82180490] Added navigation bar

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,3 +15,9 @@
 //= require turbolinks
 //= require bootstrap
 //= require_tree .
+
+
+
+
+
+
--- app/assets/javascripts/bootstrap.js
+++ app/assets/javascripts/bootstrap.js
@@ -2112,3 +2112,4 @@ if (typeof jQuery === 'undefined') { throw new Error('Bootstrap\'s JavaScript re
   })
 
 }(jQuery);
+
