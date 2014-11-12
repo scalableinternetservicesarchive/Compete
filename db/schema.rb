7177132f1644e27095038e3b906a69dfd379cc85
Alexander <alexandersvae@gmail.com>
2014-11-12 15:31:18 -0800

    Fixed a little bug i application.html

--- db/schema.rb
+++ db/schema.rb
@@ -11,9 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 20141108014536) do
-
 
   create_table "challenges", force: true do |t|
     t.string   "name"
