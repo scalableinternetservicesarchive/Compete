d4119712448fe35414a76c4c85cf34c3aa920b0b
Eirik Klevstad <Eirik.Klevstad@gmail.com>
2014-11-12 19:58:44 -0800

    Extracting name from facebook login and add to database

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141113005818) do
+ActiveRecord::Schema.define(version: 20141113035138) do
 
   create_table "challenges", force: true do |t|
     t.string   "name"
@@ -60,6 +60,7 @@ ActiveRecord::Schema.define(version: 20141113005818) do
     t.datetime "updated_at"
     t.string   "provider"
     t.string   "uid"
+    t.string   "name"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
