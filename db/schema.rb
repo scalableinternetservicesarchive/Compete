d3cfb29cee20133029fcc8a0fa68e541ba414ebe
Alexander <alexandersvae@gmail.com>
2014-11-19 17:31:26 -0800

    Added user to challenge

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141115052305) do
+ActiveRecord::Schema.define(version: 20141120004044) do
 
   create_table "challenges", force: true do |t|
     t.string   "name"
@@ -23,7 +23,10 @@ ActiveRecord::Schema.define(version: 20141115052305) do
     t.integer  "sport"
     t.integer  "distance"
     t.integer  "category"
+    t.integer  "user_id"
   end
+
+  add_index "challenges", ["user_id"], name: "index_challenges_on_user_id"
 
   create_table "comments", force: true do |t|
     t.text     "message"
