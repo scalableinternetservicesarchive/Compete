8196d706aca9938b2f49a147dfeae1f585107eb7
Eirik Klevstad <Eirik.Klevstad@gmail.com>
2014-11-27 15:58:32 -0800

    User can update profile.

--- app/controllers/user_profile_controller.rb
+++ app/controllers/user_profile_controller.rb
@@ -6,10 +6,28 @@ class UserProfileController < ApplicationController
   		@user = User.find(params[:id])
 	end
 
+	def update
+    	@user = User.find(params[:id])
+
+    	if @user.update(user_params)
+    		sign_in(@user, :bypass => true)
+      		redirect_to user_profile_path(@user.id)
+      		flash[:notice] = 'Profile updated.'
+    	else
+      		redirect_to user_profile_path(@user.id)
+    		flash[:notice] = 'Fuck'
+    	end
+  	end
+
+    private
+
+    def user_params
+    	params.require(:user).permit(:name, :location, :birthday, :email, :password, :password_confirmation, :current_password)
+    end
+
+
 	def convertToShortDate(time)
-
 		t = time.strftime("%d/%m/%Y")
-
 	end
 
 	def convertBirthdayToAge(dob)
